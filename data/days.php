b8d5dbcf54477be6bd7c160b34c3382f2f5a5297
William Italia <williamitalia70@outlook.com>
2024-05-26 00:31:47 -0300

    up

---
+++ data/days.php
@@ -0,0 +1,54 @@
+<?php
+
+$days = [
+  [
+    'data' => '27/05',
+    'refeicoes' => 4,
+    'calorias' => 3500,
+  ],
+  [
+    'data' => '28/05',
+    'refeicoes' => 3,
+    'calorias' => 3000,
+  ],
+  [
+    'data' => '29/05',
+    'refeicoes' => 2,
+    'calorias' => 2500,
+  ],
+  [
+    'data' => '30/05',
+    'refeicoes' => 3,
+    'calorias' => 2800,
+  ],
+  [
+    'data' => '31/05',
+    'refeicoes' => 4,
+    'calorias' => 3200,
+  ],
+  [
+    'data' => '01/06',
+    'refeicoes' => 2,
+    'calorias' => 2000,
+  ],
+  [
+    'data' => '02/06',
+    'refeicoes' => 3,
+    'calorias' => 2700,
+  ],
+  [
+    'data' => '03/06',
+    'refeicoes' => 2,
+    'calorias' => 2200,
+  ],
+  [
+    'data' => '04/06',
+    'refeicoes' => 3,
+    'calorias' => 2900,
+  ],
+  [
+    'data' => '05/06',
+    'refeicoes' => 2,
+    'calorias' => 2400,
+  ],
+];
---
+++ templates/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calories</title>
+    <link rel="stylesheet" href="../assets/styles/style.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+
+<body>
+    <header>
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li class="nav-item  logo"><a href="#">CALORIES</a></li>
+                <li class="nav-item active"><a href="#">Calendario</a></li>
+                <li class="nav-item"><a href="#">Imc</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#">Relatórios <i class="fa-solid fa-chevron-down"></i></a>
+                    <ul class="dropdown-content">
+                        <li><a href="">Relatório 1</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="login">
+                <li class="login-item"><a href="#">William</a></li>
+                <li class="login-item "><img class="box-img" src="../assets/img/perfil.png" alt=""></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="days-section">
+        <div class="container">
+            <div class="day">
+
+                <div class="box-day">
+                    <a href="#">
+                        <div class="informations">
+                            <div class="date">
+                                <h2>27/05</h2>
+                            </div>
+                            <div class="infos">
+                                <p>Refeições feitas: 2</p>
+                                <p>Calorias: 1500kcal</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="box-day-add">
+                    <div class="circle">
+                        <i class="icon fa-solid fa-plus"></i>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+
+    </footer>
+</body>
+
+
+
+</html>
